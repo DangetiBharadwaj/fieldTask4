d0f52176347ee2906bda0a3c4827f49759ba7467
Neil Penman <neilpenman@gmail.com>
2022-03-07 14:39:49 +1000

    no comment

--- collect_app/src/main/java/org/odk/collect/android/smap/loaders/MapLocationObserver.java
+++ collect_app/src/main/java/org/odk/collect/android/smap/loaders/MapLocationObserver.java
@@ -1,4 +1,36 @@
 package org.odk.collect.android.smap.loaders;
 
-public class MapLocationObserver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.location.Location;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.odk.collect.android.application.Collect;
+import org.odk.collect.android.smap.fragments.TaskMapFragment;
+
+
+public class MapLocationObserver extends BroadcastReceiver {
+
+    private TaskMapFragment mMap = null;
+    SharedPreferences sharedPreferences = null;
+
+public MapLocationObserver(Context context, TaskMapFragment map) {
+    mMap = map;
+
+    LocalBroadcastManager.getInstance(context).registerReceiver(this,
+            new IntentFilter("locationChanged"));
+  }
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+      Location locn = Collect.getInstance().getLocation();
+      LatLng point = new LatLng(locn.getLatitude(), locn.getLongitude());
+      mMap.updatePath(point);
+  }
 }
--- collect_app/src/main/java/org/odk/collect/android/smap/loaders/Project.java
+++ collect_app/src/main/java/org/odk/collect/android/smap/loaders/Project.java
@@ -1,4 +1,10 @@
 package org.odk.collect.android.smap.loaders;
 
+/**
+ * This class holds the lis of projects that the user has access to
+ */
 public class Project {
+    public int id;
+    public String name;
+    public String desc;
 }
