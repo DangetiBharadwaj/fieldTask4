783afc61184ecf6e03a1959dbca0a645f17502ff
Neil Penman <neilpenman@gmail.com>
2022-03-07 14:27:42 +1000

    SurveyData migration

--- collect_app/src/main/java/org/odk/collect/android/smap/loaders/SurveyData.java
+++ collect_app/src/main/java/org/odk/collect/android/smap/loaders/SurveyData.java
@@ -1,4 +1,15 @@
 package org.odk.collect.android.smap.loaders;
 
+import org.odk.collect.android.smap.loaders.PointEntry;
+import org.odk.collect.android.smap.loaders.TaskEntry;
+
+import java.util.ArrayList;
+
+/**
+ * Smap
+ * This class holds the per-item data in ViewModel
+ */
 public class SurveyData {
+    public ArrayList<TaskEntry> tasks;      // form or task
+    public ArrayList<PointEntry> points;    // form or task
 }
