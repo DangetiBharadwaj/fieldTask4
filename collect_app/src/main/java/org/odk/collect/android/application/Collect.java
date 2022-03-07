803de92c271dd38c76eb52aa1cbb9338b27b3480
Neil Penman <neilpenman@gmail.com>
2022-03-07 15:02:51 +1000

    Updates to application

--- collect_app/src/main/java/org/odk/collect/android/application/Collect.java
+++ collect_app/src/main/java/org/odk/collect/android/application/Collect.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.location.LocationManager;
 import android.os.StrictMode;
+import android.location.Location;       // smap
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,12 +29,15 @@ import androidx.multidex.MultiDex;
 
 import org.jetbrains.annotations.NotNull;
 import org.odk.collect.android.BuildConfig;
+import org.odk.collect.android.activities.FormEntryActivity;
+import org.odk.collect.android.amazonaws.mobile.AWSMobileClient;  // smap
 import org.odk.collect.android.application.initialization.ApplicationInitializer;
 import org.odk.collect.android.externaldata.ExternalDataManager;
 import org.odk.collect.android.injection.DaggerUtils;
 import org.odk.collect.android.injection.config.AppDependencyComponent;
 import org.odk.collect.android.injection.config.DaggerAppDependencyComponent;
 import org.odk.collect.android.javarosawrapper.FormController;
+import org.odk.collect.android.smap.loaders.GeofenceEntry;
 import org.odk.collect.android.preferences.screens.MapsPreferencesFragment;
 import org.odk.collect.android.utilities.FormsRepositoryProvider;
 import org.odk.collect.android.utilities.LocaleHelper;
@@ -67,6 +71,8 @@ import org.odk.collect.shared.settings.Settings;
 import org.odk.collect.shared.strings.Md5;
 import org.odk.collect.strings.localization.LocalizedApplication;
 
+import java.util.ArrayList;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.util.Locale;
@@ -90,6 +96,19 @@ public class Collect extends Application implements
     private FormController formController;
     private ExternalDataManager externalDataManager;
     private AppDependencyComponent applicationComponent;
+
+    private Location location = null;                   // smap
+    private Location savedLocation = null;              // Location saved to trail database
+    private ArrayList<GeofenceEntry> geofences = new ArrayList<GeofenceEntry>();    // smap
+    private boolean tasksDownloading = false;           // smap
+    // Keep a reference to form entry activity to allow cancel dialogs to be shown during remote calls
+    private FormEntryActivity formEntryActivity = null; // smap
+    private HashMap<String, SmapRemoteDataItem> remoteCache = null;         // smap
+    private int remoteCalls;                                                // smap
+    private Stack<FormLaunchDetail> formStack = new Stack<>();              // smap
+    private FormRestartDetails mRestartDetails;                             // smap
+    private String formId;                                                  // smap
+    private String searchLocalData;                                         // smap
 
     @Inject
     ApplicationInitializer applicationInitializer;
@@ -209,6 +228,136 @@ public class Collect extends Application implements
         //noinspection deprecation
         defaultSysLanguage = newConfig.locale.getLanguage();
     }
+
+    // Begin Smap
+    public void setFormId(String v) {
+        formId = v;
+    }
+    public String getFormId() {
+        return formId;
+    }
+    public void setSearchLocalData(String v) {
+        searchLocalData = v;
+    }
+    public String getSearchLocalData() {
+        return searchLocalData;
+    }
+
+    public void setLocation(Location l) {
+        location = l;
+    }
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setSavedLocation(Location l) {
+        savedLocation = l;
+    }
+    public Location getSavedLocation() {
+        return savedLocation;
+    }
+
+    public void setGeofences(ArrayList<GeofenceEntry> geofences) {
+        this.geofences = geofences;
+    }
+    public ArrayList<GeofenceEntry> getGeofences() {
+        return geofences;
+    }
+
+    public void setDownloading(boolean v) {
+        tasksDownloading = v;
+    }
+    public boolean  isDownloading() {
+        return tasksDownloading;
+    }
+    // Initialise AWS
+    private void initializeApplication() {
+        AWSMobileClient.initializeMobileClientIfNecessary(getApplicationContext());
+
+        // ...Put any application-specific initialization logic here...
+    }
+    // Set form entry activity
+    public void setFormEntryActivity(FormEntryActivity activity) {
+        formEntryActivity = activity;
+    }
+    public FormEntryActivity getFormEntryActivity() {
+        return formEntryActivity;
+    }
+    public void clearRemoteServiceCaches() {
+        remoteCache = new HashMap<String, SmapRemoteDataItem>();
+    }
+    public void initRemoteServiceCaches() {
+        if(remoteCache == null) {
+            remoteCache = new HashMap<String, SmapRemoteDataItem>();
+        } else {
+            ArrayList<String> expired = new ArrayList<String>();
+            for(String key : remoteCache.keySet()) {
+                SmapRemoteDataItem item = remoteCache.get(key);
+                if(item.perSubmission) {
+                    expired.add(key);
+
+                }
+            }
+            if(expired.size() > 0) {
+                for(String key : expired) {
+                    remoteCache.remove(key);
+                }
+            }
+        }
+        remoteCalls = 0;
+    }
+    public String getRemoteData(String key) {
+        SmapRemoteDataItem item = remoteCache.get(key);
+        if(item != null) {
+            return item.data;
+        } else {
+            return null;
+        }
+    }
+    public void setRemoteItem(SmapRemoteDataItem item) {
+        if(item.data == null) {
+            // There was a network error
+            remoteCache.remove(item.key);
+        } else {
+            remoteCache.put(item.key, item);
+        }
+    }
+    public void startRemoteCall() {
+        remoteCalls++;
+    }
+    public void endRemoteCall() {
+        remoteCalls--;
+    }
+    public boolean inRemoteCall() {
+        return remoteCalls > 0;
+    }
+
+    public void setFormRestartDetails(FormRestartDetails restartDetails) {
+        mRestartDetails = restartDetails;
+    }
+    public FormRestartDetails getFormRestartDetails() {
+        return mRestartDetails;
+    }
+
+    /*
+     * Push a FormLaunchDetail to the stack
+     * this form should then be launched by SmapMain
+     */
+    public void pushToFormStack(FormLaunchDetail fld) {
+        formStack.push(fld);
+    }
+
+    /*
+     * Pop a FormLaunchDetails from the stack
+     */
+    public FormLaunchDetail popFromFormStack() {
+        if(formStack.empty()) {
+            return null;
+        } else {
+            return formStack.pop();
+        }
+    }
+    // End Smap
 
     public AppDependencyComponent getComponent() {
         return applicationComponent;
